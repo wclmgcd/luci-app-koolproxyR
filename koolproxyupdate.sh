0566482de3fac34ea566665b375e3dc381ed2fec
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2020-09-06 12:13:06 +0800

    二进制文件更新

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -7,6 +7,14 @@ wget 'https://raw.fastgit.org/fw869/AD/master/user.txt' -O files/usr/share/koolp
 cp files/usr/share/koolproxy/data/user.txt files/usr/share/koolproxy/data/rules/user.txt
 wget 'https://raw.fastgit.org/fw869/AD/master/koolproxy_ipset.conf' -O files/usr/share/koolproxy/koolproxy_ipset.conf
 
+wget 'https://kprule.com/user.txt' -O files/usr/share/koolproxy/data/user.txt
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/arm' -O files/bin/arm
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/i386' -O files/bin/i386
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/mips' -O files/bin/mips
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/mipsel' -O files/bin/mipsel
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/x86_64' -O files/bin/x86_64
+chmod +x files/bin/*
+
 wget https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O- | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > files/usr/share/koolproxy/dnsmasq.adblock
 sed -i '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
 sed -i '/[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}/d' files/usr/share/koolproxy/dnsmasq.adblock
